d159c08bd86101f1c5927502cc20a3b599b92ad5
vidhi-arora <73910280+vidhi-arora@users.noreply.github.com>
2020-12-19 01:04:49 +0530

    Update contact_form.php

--- contact_form.php
+++ contact_form.php
@@ -1,30 +1,16 @@
 <?php
-$user_name = $user_email = $message = "";
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $user_name = test_input($_POST["Name"]);
-  $user_email = test_input($_POST["email"]);
-  $message = test_input($_POST["message"]);
-}
+$user_name= $_POST["Name"];
+$user_email= $_POST["email"];
+$message= $_POST["message"];
 
 $email_from= "csvidhi15@gmail.com";
-$subject= "Message from my artportfolio";
+$subject= "Message from my ArtPortfolio";
 $body= "Name: $user_name.\n".
 		"Email ID: $user_email.\n".
 		"Message: \n $message";
 
 $to= "csvidhi15@gmail.com";
 
-$headers= "From: $email_from \r\n";
-$headers.= "Reply To: $user_email \r\n";
-
-
-mail($to,$subject,$body,$headers);
+mail($to,$subject,$body);
 header('Location: contact.html');
 ?>
